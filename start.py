5588d3fcc6b2e10dcbd414b8182429d17f92a9fb
pascal <pascal.salamon@uni-konstanz.de>
2018-06-22 17:00:06 +0200

    bilder in html document anzeigen

--- start.py
+++ start.py
@@ -1,11 +1,13 @@
 import os
+import re
+
 from flask import Flask, render_template, request
 
 import controller
 import db_connection
 from mmdbs_image import MMDBSImage
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/'
 
 
@@ -16,8 +18,9 @@ def start():
     sim = None
     seg = None
     eigenval = None
+    similiar_objects = None
     picanz = db_connection.get_count_images()
-    return callHtmlPage(feat, sim, seg, eigenval, picanz, queryobject)
+    return callHtmlPage(feat, sim, seg, eigenval, picanz, queryobject, similiar_objects)
 
 
 @app.route('/do_db_search', methods=['POST', 'GET'])
@@ -42,9 +45,12 @@ def do_db_search():
         temp_image = MMDBSImage()
         temp_image.set_image(thePath, '')
         similiar_objects = controller.get_similar_objects(temp_image, feature, seg)
+        for similiar_object in similiar_objects:
+            path = similiar_object['mmdbs_image'].path
+            pattern = re.compile('/static.+')
+            similiar_object['mmdbs_image'].path = pattern.findall(path)[0]
+        return callHtmlPage(feature, sim, seg, eigenval, picanz, queryobject, similiar_objects)
 
-        return callHtmlPage(feature, sim, seg, eigenval, picanz, queryobject)
 
-
-def callHtmlPage(feat, sim, seg, eigenanz, picanz, qo):
-    return render_template('index.html', feat=feat, sim=sim, seg=seg, eigenanz=eigenanz, picanz=picanz, qo=qo)
+def callHtmlPage(feat, sim, seg, eigenanz, picanz, qo, so):
+    return render_template('index.html', feat=feat, sim=sim, seg=seg, eigenanz=eigenanz, picanz=picanz, qo=qo, so=so)
