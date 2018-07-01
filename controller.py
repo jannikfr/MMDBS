8c7737601ab3d7a07c2fa19dbfa4c77e40fdf1fc
Jannik Frauendorf <jannik.frauendorf@gmail.com>
2018-07-01 12:33:21 +0200

    Code cleanup

--- controller.py
+++ controller.py
@@ -1,6 +1,3 @@
-import math
-
-import cv2
 from operator import itemgetter
 import numpy as np
 import db_connection
@@ -12,7 +9,6 @@ class Controller(object):
     def __init__(self):
         self.conn = db_connection.connect()
         self.mmdbs_data = db_connection.get_all_images(self.conn)
-
 
     def extract_all_features(self, mmdbs_image):
         mmdbs_image.global_histogram = self.extract_global_histogram_feature(mmdbs_image)
@@ -66,7 +62,8 @@ class Controller(object):
         # loop over all images
         for mmdbs_image in all_mmdbs_images:
             # get distance between query object and mmdbs_image for this parameter and append it to the list
-            similar_objects.append(self.get_distance(query_object_feature, feature, seg, distance_function, mmdbs_image))
+            similar_objects.append(
+                self.get_distance(query_object_feature, feature, seg, distance_function, mmdbs_image))
 
         return similar_objects
 
@@ -77,8 +74,8 @@ class Controller(object):
         # read the selected feature from the mmdbs_image (database object)
         mmdbs_image_feature = self.get_mmdbs_image_feature(feature, seg, mmdbs_image)
         # build the mmdbs_image_distance dic with mmdbs_image:distance
-        return self.get_mmdbs_image_distance_dictonary(mmdbs_image_feature, query_object_feature, distance_function,
-                                                    mmdbs_image, feature)
+        return self.get_mmdbs_image_distance_dictionary(mmdbs_image_feature, query_object_feature, distance_function,
+                                                        mmdbs_image, feature)
 
     def get_mmdbs_image_feature(self, feature, seg, mmdbs_image):
         # read the selected feature from the mmdbs_image
@@ -110,8 +107,8 @@ class Controller(object):
         elif feature == 'contours':
             return mmdbs_image.contours
 
-    def get_mmdbs_image_distance_dictonary(self, mmdbs_image_feature, query_object_feature, distance_function,
-                                           mmdbs_image, feature):
+    def get_mmdbs_image_distance_dictionary(self, mmdbs_image_feature, query_object_feature, distance_function,
+                                            mmdbs_image, feature):
         # initialize dic
         mmdbs_image_distance_dictonary = {}
         # set image as key
@@ -121,7 +118,8 @@ class Controller(object):
         query_object_feature_value = self.get_value_for_distance_calculation(query_object_feature, feature)
 
         # call distance function for calculation
-        distance = distance_calculator.calculate_distance(mmdbs_image_feature_value, query_object_feature_value, distance_function)
+        distance = distance_calculator.calculate_distance(mmdbs_image_feature_value, query_object_feature_value,
+                                                          distance_function)
         # set distance as value
         mmdbs_image_distance_dictonary['distance'] = distance
 
@@ -137,35 +135,33 @@ class Controller(object):
         # extract local histogram feature corresponding to segmentation parameter
         if seg == '2_2':
             mmdbs_image.local_histogram_2_2 = mmdbs_image.extract_histograms(mmdbs_image.image, 2, 2, [8, 2, 4],
-                                                                               False)
+                                                                             False)
             return mmdbs_image.local_histogram_2_2
 
         elif seg == '3_3':
             mmdbs_image.local_histogram_3_3 = mmdbs_image.extract_histograms(mmdbs_image.image, 3, 3, [8, 2, 4],
-                                                                               False)
+                                                                             False)
             return mmdbs_image.local_histogram_3_3
 
         elif seg == '4_4':
             mmdbs_image.local_histogram_4_4 = mmdbs_image.extract_histograms(mmdbs_image.image, 4, 4, [8, 2, 4],
-                                                                               False)
+                                                                             False)
             return mmdbs_image.local_histogram_4_4
-
 
     def extract_global_histogram_feature(self, mmdbs_image):
         mmdbs_image.global_histogram = mmdbs_image.extract_histograms(mmdbs_image.image, 1, 1, [8, 2, 4], False)
         return mmdbs_image.global_histogram
-
 
     def extract_global_edge_histogram_feature(self, mmdbs_image):
         mmdbs_image.sobel_edges = self.extract_sobel_edges(mmdbs_image)
         min_edge_value = np.min(mmdbs_image.sobel_edges)
         max_edge_value = np.max(mmdbs_image.sobel_edges)
         mmdbs_image.global_edge_histogram = mmdbs_image.extract_histograms_one_channel(mmdbs_image.sobel_edges,
-                                                                                         1, 1,
-                                                                                         64,
-                                                                                         False,
-                                                                                         min_edge_value,
-                                                                                         max_edge_value)
+                                                                                       1, 1,
+                                                                                       64,
+                                                                                       False,
+                                                                                       min_edge_value,
+                                                                                       max_edge_value)
         return mmdbs_image.global_edge_histogram
 
     def extract_global_hue_histogram_feature(self, mmdbs_image):
@@ -175,7 +171,6 @@ class Controller(object):
         mmdbs_image.global_hue_histogram = mmdbs_image.extract_histograms_one_channel(h_image, 1, 1, 64, False,
                                                                                       min_h_value, max_h_value)
         return mmdbs_image.global_hue_histogram
-
 
     def extract_color_moments_feature(self, mmdbs_image):
         mmdbs_image.color_moments = mmdbs_image.extract_color_moments(mmdbs_image.image)
